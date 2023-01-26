15f2180419219557050117e0528988208c146f38
0KEJJ <night21004@gmail.com>
2023-01-27 00:43:15 +0100

    a

---
+++ docs/_next/static/ytN6Ey3Fun9N0zz5yk8G0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f6a14a88c1c59ae7.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/index2":["static/chunks/pages/index2-7ddf14a99f6a1afe.js"],sortedPages:["/","/_app","/_error","/index2"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
